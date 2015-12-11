f81d46c0df6adc7a3852f3ebafe3d2ca0e183197
kenny mcgarvey <kenny.mcgarvey@gmail.com>
2015-12-11 15:27:28 -0600

    Rebuilds dist files.
    Signed-off-by: kenny mcgarvey <kenny.mcgarvey@gmail.com>

--- dist/backbone-validation.js
+++ dist/backbone-validation.js
@@ -46,18 +46,35 @@ Backbone.Validation = (function(_){
   // eg:
   //
   //     var o = {
-  //       address: {
-  //         street: 'Street',
-  //         zip: 1234
+  //       owner: {
+  //         name: 'Backbone',
+  //         address: {
+  //           street: 'Street',
+  //           zip: 1234
+  //         }
   //       }
   //     };
   //
   // becomes:
   //
   //     var o = {
-  //       'address.street': 'Street',
-  //       'address.zip': 1234
+  //       'owner': {
+  //         name: 'Backbone',
+  //         address: {
+  //           street: 'Street',
+  //           zip: 1234
+  //         }
+  //       },
+  //       'owner.name': 'Backbone',
+  //       'owner.address': {
+  //         street: 'Street',
+  //         zip: 1234
+  //       },
+  //       'owner.address.street': 'Street',
+  //       'owner.address.zip': 1234
   //     };
+  // This may seem redundant, but it allows for maximum flexibility
+  // in validation rules.
   var flatten = function (obj, into, prefix) {
     into = into || {};
     prefix = prefix || '';
@@ -67,9 +84,9 @@ Backbone.Validation = (function(_){
         if (!!val && typeof val === 'object' && val.constructor === Object) {
           flatten(val, into, prefix + key + '.');
         }
-        else {
-          into[prefix + key] = val;
-        }
+
+        // Register the current level object as well
+        into[prefix + key] = val;
       }
     });
 
@@ -415,7 +432,7 @@ Backbone.Validation = (function(_){
     digits: /^\d+$/,
 
     // Matches any number (e.g. 100.000)
-    number: /^-?(?:\d+|\d{1,3}(?:,\d{3})+)(?:\.\d+)?$/,
+    number: /^-?(?:\d+|\d{1,3}(?:,\d{3})+)?(?:\.\d*)?$/,
 
     // Matches a valid email address (e.g. mail@example.com)
     email: /^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))$/i,
